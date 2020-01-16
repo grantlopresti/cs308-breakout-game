b157449f8b976fb0bf3335743e9d8b90c4c5e3e7
Grant LoPresti <gjl13@duke.edu>
2020-01-15 22:30:54 -0500

    Ball bounces off of moving paddle

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -22,7 +22,7 @@ import javafx.util.Duration;
  * @author Robert C. Duvall
  */
 public class Main extends Application {
-    public static final String TITLE = "Example JavaFX";
+    public static final String TITLE = "BREAKOUT (gjl13)";
     public static final int SIZE = 400;
     public static final int FRAMES_PER_SECOND = 60;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
@@ -31,10 +31,10 @@ public class Main extends Application {
     public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
     public static final String BOUNCER_IMAGE = "ball.gif";
     public static final String PADDLE_IMAGE = "paddle.gif";
-    public static final int BOUNCER_SPEED = 30;
+    public static final int PADDLE_SPEED = 5;
+    public static final int BOUNCER_SPEED = 2;
     public static final Paint MOVER_COLOR = Color.PLUM;
     public static final int MOVER_SIZE = 50;
-    public static final int MOVER_SPEED = 5;
     public static final Paint GROWER_COLOR = Color.BISQUE;
     public static final double GROWER_RATE = 1.1;
     public static final int GROWER_SIZE = 50;
@@ -42,6 +42,9 @@ public class Main extends Application {
     // some things needed to remember during game
     private Scene myScene;
     private ImageView myBouncer;
+    private ImageView myPaddle;
+    public static double bouncerXDir = 1;
+    public static double bouncerYDir = -1;
     private Rectangle myMover;
     private Rectangle myGrower;
 
@@ -69,11 +72,15 @@ public class Main extends Application {
         // create one top level collection to organize the things in the scene
         Group root = new Group();
         // make some shapes and set their properties
-        Image image = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
-        myBouncer = new ImageView(image);
+        Image bouncerImage = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
+        myBouncer = new ImageView(bouncerImage);
+        Image paddleImage = new Image(this.getClass().getClassLoader().getResourceAsStream(PADDLE_IMAGE));
+        myPaddle = new ImageView(paddleImage);
         // x and y represent the top left corner, so center it in window
         myBouncer.setX(width / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
-        myBouncer.setY(height / 2 - myBouncer.getBoundsInLocal().getHeight() / 2);
+        myBouncer.setY(height - myPaddle.getBoundsInLocal().getHeight() - myBouncer.getBoundsInLocal().getHeight() /2);
+        myPaddle.setX(width / 2 - myPaddle.getBoundsInLocal().getWidth() / 2);
+        myPaddle.setY(height - myPaddle.getBoundsInLocal().getHeight());
         myMover = new Rectangle(width / 2 - MOVER_SIZE / 2, height / 2 - 100, MOVER_SIZE, MOVER_SIZE);
         myMover.setFill(MOVER_COLOR);
         myGrower = new Rectangle(width / 2 - GROWER_SIZE / 2, height / 2 + 50, GROWER_SIZE, GROWER_SIZE);
@@ -82,6 +89,7 @@ public class Main extends Application {
         root.getChildren().add(myBouncer);
         root.getChildren().add(myMover);
         root.getChildren().add(myGrower);
+        root.getChildren().add(myPaddle);
         // create a place to see the shapes
         Scene scene = new Scene(root, width, height, background);
         // respond to input
@@ -94,7 +102,7 @@ public class Main extends Application {
     // Note, there are more sophisticated ways to animate shapes, but these simple ways work fine to start
     private void step (double elapsedTime) {
         // update "actors" attributes
-        myBouncer.setX(myBouncer.getX() + BOUNCER_SPEED * elapsedTime);
+        checkWallCollision();
         myMover.setRotate(myMover.getRotate() - 1);
         myGrower.setRotate(myGrower.getRotate() + 1);
 
@@ -109,28 +117,45 @@ public class Main extends Application {
         else {
             myMover.setFill(MOVER_COLOR);
         }
-        // with images can only check bounding box
-        if (myGrower.getBoundsInParent().intersects(myBouncer.getBoundsInParent())) {
-            myGrower.setFill(HIGHLIGHT);
+        // with images can only check bounding
+        checkPaddleCollision();
+    }
+
+    private void checkWallCollision(){
+        double minX = myBouncer.getBoundsInParent().getMinX();
+        double maxX = myBouncer.getBoundsInParent().getMaxX();
+        double minY = myBouncer.getBoundsInParent().getMinY();
+        double maxY = myBouncer.getBoundsInParent().getMaxY();
+
+        if (maxX >= SIZE || minX <= 0){
+            bouncerXDir = -1*bouncerXDir;
         }
-        else {
-            myGrower.setFill(GROWER_COLOR);
+        if (minY <= 0 ){
+            bouncerYDir = -1*bouncerYDir;
+        }
+        if (maxY >= SIZE){
+            bouncerXDir = 0;
+            bouncerYDir = 0;
+        }
+
+        myBouncer.setX(myBouncer.getX() + BOUNCER_SPEED*bouncerXDir);
+        myBouncer.setY(myBouncer.getY() + BOUNCER_SPEED*bouncerYDir);
+    }
+
+    private void checkPaddleCollision () {
+        if (myBouncer.getBoundsInParent().intersects(myPaddle.getBoundsInParent())){
+            bouncerYDir = -1*bouncerYDir;
+            myBouncer.setY(myBouncer.getY() + BOUNCER_SPEED*bouncerYDir);
         }
     }
 
     // What to do each time a key is pressed
     private void handleKeyInput (KeyCode code) {
         if (code == KeyCode.RIGHT) {
-            myMover.setX(myMover.getX() + MOVER_SPEED);
+            myPaddle.setX(myPaddle.getX() + PADDLE_SPEED);
         }
         else if (code == KeyCode.LEFT) {
-            myMover.setX(myMover.getX() - MOVER_SPEED);
-        }
-        else if (code == KeyCode.UP) {
-            myMover.setY(myMover.getY() - MOVER_SPEED);
-        }
-        else if (code == KeyCode.DOWN) {
-            myMover.setY(myMover.getY() + MOVER_SPEED);
+            myPaddle.setX(myPaddle.getX() - PADDLE_SPEED);
         }
         // NEW Java 12 syntax that some prefer (but watch out for the many special cases!)
         //   https://blog.jetbrains.com/idea/2019/02/java-12-and-intellij-idea/
