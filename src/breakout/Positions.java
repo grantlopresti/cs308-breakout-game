83665d877f59529cc706e3b812827f92631f7581
Grant LoPresti <gjl13@duke.edu>
2020-01-19 00:35:29 -0500

    added positions class

---
+++ src/breakout/Positions.java
@@ -0,0 +1,13 @@
+public class Positions {
+    double xMin;
+    double xMax;
+    double yMin;
+    double yMax;
+    Positions(double myXmin, double myXmax, double myYmin, double myYmax)
+    {
+        xMin = myXmin;
+        xMax = myXmax;
+        yMin = myYmin;
+        yMax = myYmax;
+    }
+}
